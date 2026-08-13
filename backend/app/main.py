598bbc1047898be7f2763b89842fd8c05a6b66b8
IsaacDev <fatokunboi@gmail.com>
2026-08-13 16:54:14 +0100

    added hosted site to the  CORS origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,6 +9,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://habit-coach-beige.vercel.app"
         "http://localhost:8080",
         "http://127.0.0.1:8080",
         "http://localhost:5173",
